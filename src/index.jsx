13d298e8d289ddcc3708667bf612b486ebbac5de
VanesMP <vanessa.MP@outlook.fr>
2022-06-21 11:08:02 +0200

    last changes css

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import { Modal } from "../src/lib/Modal.jsx";
 import "../src/lib/modal.css";
 
-ReactDOM.render(<Modal />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+      <Modal />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
