4ef8f81e10299c30aeff93383de308dd9fec879e
VanesMP <vanessa.MP@outlook.fr>
2022-06-21 10:49:10 +0200

    last changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Modal } from '../src/lib/Modal.jsx';
-import "../src/lib/Modal.css";
+import "../src/lib/modal.css";
 
 ReactDOM.render(<Modal />, document.getElementById('root'));
--- src/lib/Modal.jsx
+++ src/lib/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../lib/modal.css"
 
 /**Render of the modal when the form is validated
  * @function Modal
